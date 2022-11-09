f69956df9a68750217621a57ec0a074dd9199d67
bbuyatti <buyattib29@gmail.com>
2022-11-09 18:32:16 -0300

    cambie la definicion de la red gt3

--- src/preProcessing/make_gt3_and_lt3_edges.py
+++ src/preProcessing/make_gt3_and_lt3_edges.py
@@ -4,7 +4,7 @@ data_dir = "./data"
 edges_dir = data_dir + "/edges"
 
 edges = np.load(edges_dir + "/all_edges.npy")
-gt3 = edges[edges[:,2] >= 3][:,:2]
+gt3 = edges[edges[:,2] > 3][:,:2]
 lt3 = edges[edges[:,2] < 3][:,:2]
 
 np.save(edges_dir + "/gt3.npy", gt3)
